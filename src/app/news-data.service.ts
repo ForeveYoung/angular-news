303f24ede2543c321ee320ceac8cbb2eaed54058
ForeveYang <hrrragon@gmail.com>
2021-05-05 01:41:03 +0300

    fixed request

--- src/app/news-data.service.ts
+++ src/app/news-data.service.ts
@@ -10,7 +10,7 @@ export class NewsDataService {
   constructor(private http:HttpClient) { }
 
   getData():Observable<any>{
-    return this.http.get('https://gnews.io/api/v4/search?q=example&token=1d9437483745e5176562c6d2cdeceaf7')
+    return this.http.get('https://gnews.io/api/v4/search?q=example&token=1cb4013126aae4762c5ac1419da40a8d')
 
   }
 }
